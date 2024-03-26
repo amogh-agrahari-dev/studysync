e3811e52b1cbae39698f47cef939e6c0a5affd59
amogh-arahari-dev <amogh.agrahari.dev@gmail.com>
2024-03-26 17:09:03 +0530

    Tasks done

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
     <div className='bg-[#0f0f0f] text-white p-10 rounded-xl flex flex-col justify-center'>
       <img className='rounded-full w-60 h-60 text-center mx-auto mb-2' src="https://cdn-icons-png.flaticon.com/512/4515/4515017.png" alt="" />
       <span className="text-4xl mb-5 font-bold text-center" >STUDYSYNC</span>
-      <span className="text-2xl font-bold text-center" >The Only Productivity App You Need</span>
+      <span className="text-3xl font-bold text-center" >The Only Productivity App You Need</span>
       <div className='mt-5 mx-auto'>
       <button className="bg-blue-600 hover:bg-blue-800 text-white font-semibold text-2xl w-full px-8 py-2 rounded-lg mx-auto" onClick={() => loginHandler()}>Sign In With Google</button>
     </div>
